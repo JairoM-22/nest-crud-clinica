16d1a22c35d4773fb673db1436696d4e5537e898
Jairo <aconeoj@uninorte.edu.co>
2026-07-07 10:26:50 -0500

    entity

--- src/pacientes/entities/paciente.entity.ts
+++ src/pacientes/entities/paciente.entity.ts
@@ -1,1 +1,29 @@
-export class Paciente {}
+import { Timestamp } from "typeorm/driver/mongodb/bson.typings.js"
+import {Column,Entity,OneToMany ,PrimaryGeneratedColumn,CreateDateColumn} from 'typeorm'  
+import { IsString, IsEmail,MinLength, IsDate } from 'class-validator';
+
+@Entity({name: 'products'})
+export class Paciente {
+    @PrimaryGeneratedColumn()
+    id!:string
+
+
+    @IsString()
+    @MinLength(1)
+    nombre!:string
+
+    @IsEmail()
+    @MinLength(1)  
+    correo!:string
+
+    @IsString()
+    @MinLength(1)
+    telefono!:string
+
+    @IsDate()
+    @MinLength(1)
+    fecha_nacimiento!:Date
+
+    @CreateDateColumn({ type: 'timestamp', name: 'fecha_registro' })
+    fechaRegistro!: Date;
+}
