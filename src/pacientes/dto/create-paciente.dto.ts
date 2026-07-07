6544440b96d806c372291d8078923c8d3226b8c8
Jairo <aconeoj@uninorte.edu.co>
2026-07-07 10:50:04 -0500

    entity

--- src/pacientes/dto/create-paciente.dto.ts
+++ src/pacientes/dto/create-paciente.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePacienteDto {}
+import { IsString, IsEmail,MinLength, IsDate } from 'class-validator';
+
+export class CreatePacienteDto {
+
+    id!:string
+
+
+    @isString()
+    nombre!:string
+
+    @Column()  
+    correo!:string
+
+    @Column()
+    telefono!:string
+
+    @Column()
+    fecha_nacimiento!:Date
+
+    @CreateDateColumn({ type: 'timestamp', name: 'fecha_registro' })
+    fechaRegistro!: Date;
+}
+
+
