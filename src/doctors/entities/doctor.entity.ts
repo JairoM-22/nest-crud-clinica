a8a8ee133e72898d606ab370d40af1d2426446bb
Jcontrerasm <126794784+PachitoBott@users.noreply.github.com>
2026-07-07 10:23:24 -0500

    cita, doctor, receta

--- src/doctors/entities/doctor.entity.ts
+++ src/doctors/entities/doctor.entity.ts
@@ -1,1 +1,30 @@
-export class Doctor {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Doctor {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column('text', {
+        unique: true,
+        nullable:false,
+    })
+    nombre!: string;    
+
+
+    @Column('text', {
+        unique: true,
+    })
+    correo!: string;  
+    
+    
+    @Column('text', {
+        unique: true,
+    })
+    telefono!: string;
+
+    @Column('int', {
+        unique: true,
+    })
+    numero_consultorio!: number;
+}
--- src/recetas/entities/receta.entity.ts
+++ src/recetas/entities/receta.entity.ts
@@ -1,1 +1,32 @@
-export class Receta {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+
+export class Receta {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+
+    @Column('int')
+    cita_id!: number;
+
+    
+    @Column('text',{
+        nullable:false
+    })
+    medicamento!: string;
+
+    @Column('text',{
+        nullable:false
+    })
+    dosis!: string;
+
+    @Column('text')
+    instruccion!: string;
+
+    @CreateDateColumn({
+            type: 'timestamp',
+    })
+   fecha_emision!: Date;
+
+
+}
